14d1f7f4d9996559fdf424cfa62bfe2b48323321
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-01 11:55:46 -0300

    feat: criando component search

---
+++ src/components/search.js
@@ -0,0 +1,12 @@
+'use strict';
+import React from 'react';
+
+const Search = () => {
+	return (
+		<div className="search box-wrapper">
+			<input type="search" placeholder="Digite o nome do usuário do Github" />
+		</div>
+	);
+};
+
+export default Search;
