eff8d4845e221ff9ca406919401851d986d1a127
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-01 16:23:00 -0300

    feat: criando o objeto de estado da aplicação

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import AppContent from './components/app-content';
 class App extends React.Component {
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			userInfo: {
+				username: 'Daniel Silva',
+				userimg: 'https://avatars1.githubusercontent.com/u/16245261?v=4',
+				userlogin: 'daniel-silva-dxp',
+				userrepos: 45,
+				userfollowers: 4,
+				userfollowing: 15
+			},
+			repos: [ { name: 'Nome do repositório', link: '#' } ],
+			starred: [ { name: 'Nome do repositório', link: '#' } ]
+		};
 	}
 	render() {
-		return <AppContent />;
+		return <AppContent userInfo={this.state.userInfo} repos={this.state.repos} starred={this.state.starred} />;
 	}
 }
 
