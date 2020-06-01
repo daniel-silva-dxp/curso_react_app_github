915b40e2b05b5b161b66260c56abd849fc902ace
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-01 11:57:09 -0300

    feat: criando component actions

---
+++ src/components/actions.js
@@ -0,0 +1,13 @@
+'use strict';
+import React from 'react';
+
+const Actions = () => {
+	return (
+		<div className="actions">
+			<button className="btn btn-medium color-btn">Ver repositórios</button>
+			<button className="btn btn-medium color-btn">Ver favoritos</button>
+		</div>
+	);
+};
+
+export default Actions;
