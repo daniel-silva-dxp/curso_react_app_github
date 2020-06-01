b7b81e2cd646e78f55d10fd3fc8e537401011dc5
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-01 11:58:24 -0300

    feat: removendo markup e importando components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,63 @@
-'user stric';
+'use strict';
 import React from 'react';
+import Search from './components/search';
+import UserInfo from './components/user-info';
+import Actions from './components/actions';
+import Repos from './components/repos';
 
 // Renderizando componentes utilizando Classes
 class App extends React.Component {
-
 	render() {
 		return (
-			<div className='app'>
-			<div className='search'>
-				<input 
-					type='search' 
-					placeholder="Digite o nome do usuário no Github"
-				/>
-			</div>
-				<div className='user-info'>
-					<img src='https://avatars1.githubusercontent.com/u/16245261?v=4' />
-					<h1>
-						<a href="https://github.com/daniel-silva-dxp">
-							Daniel Silva
-						</a>
-					</h1>
-					<ul className="repos-info">
-						<li>Repositórios: 41</li>
-						<li>Seguidores: 4</li>
-						<li>Seguindo: 15</li>
-					</ul>
-					<div className='actions'>
-						<button>Ver repositórios</button>
-						<button>Ver favoritos</button>
-					</div>
-					<div className="repos">
-						<h2>Repositórios:</h2>
-						<ul>
-							<li>
-								<a href='#'>
-									Nome do repositório
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div className="starred">
-						<h2>Favoritos:</h2>
-						<ul>
-							<li>
-								<a href='#'>
-									Nome do repositório
-								</a>
-							</li>
-						</ul>
-					</div>
+			<div className="app">
+				<Search />
+				<div className="container">
+					<UserInfo />
+					<Actions />
+					<Repos
+						className="repos box-wrapper"
+						title="Repositório:"
+						repos={[
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							},
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							},
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							},
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							}
+						]}
+					/>
+					<Repos
+						className="starred box-wrapper"
+						title="Favoritos:"
+						repos={[
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							},
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							},
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							},
+							{
+								name: 'Nome do repositório',
+								link: '#'
+							}
+						]}
+					/>
 				</div>
 			</div>
 		);
