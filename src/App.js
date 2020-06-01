62974b7852e98f6e75f7735964e7b1eca9185238
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-01 14:33:17 -0300

    feat: criando container componet da aplicação

--- src/App.js
+++ src/App.js
@@ -1,66 +1,14 @@
 'use strict';
 import React from 'react';
-import Search from './components/search';
-import UserInfo from './components/user-info';
-import Actions from './components/actions';
-import Repos from './components/repos';
+import AppContent from './components/app-content';
 
-// Renderizando componentes utilizando Classes
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
 	render() {
-		return (
-			<div className="app">
-				<Search />
-				<div className="container">
-					<UserInfo />
-					<Actions />
-					<Repos
-						className="repos box-wrapper"
-						title="Repositório:"
-						repos={[
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							},
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							},
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							},
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							}
-						]}
-					/>
-					<Repos
-						className="starred box-wrapper"
-						title="Favoritos:"
-						repos={[
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							},
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							},
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							},
-							{
-								name: 'Nome do repositório',
-								link: '#'
-							}
-						]}
-					/>
-				</div>
-			</div>
-		);
+		return <AppContent />;
 	}
 }
 
