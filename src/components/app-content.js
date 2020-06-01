219234c35e5bd8d9d5bd3b33fc863f5edcd902a9
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-01 16:23:27 -0300

    feat: recebendo o objecto state

--- src/components/app-content.js
+++ src/components/app-content.js
@@ -1,40 +1,30 @@
 'use strict';
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Search from './search';
 import UserInfo from './user-info';
 import Actions from './actions';
 import Repos from './repos';
 
-const AppContent = () => {
+const AppContent = ({ userInfo, repos, starred }) => {
 	return (
 		<div className="app">
 			<Search />
 			<div className="container">
-				<UserInfo />
-				<Actions />
-				<Repos
-					className="repos box-wrapper"
-					title="Repositório:"
-					repos={[
-						{
-							name: 'Nome do repositório',
-							link: '#'
-						}
-					]}
-				/>
-				<Repos
-					className="starred box-wrapper"
-					title="Favoritos:"
-					repos={[
-						{
-							name: 'Nome do repositório',
-							link: '#'
-						}
-					]}
-				/>
+				{!!userInfo && <UserInfo userInfo={userInfo} />}
+				{!!userInfo && <Actions />}
+
+				{!!repos.length && <Repos className="repos box-wrapper" title="Repositório:" repos={repos} />}
+
+				{!!starred.length && <Repos className="starred box-wrapper" title="Favoritos:" repos={starred} />}
 			</div>
 		</div>
 	);
+};
+
+AppContent.propType = {
+	userInfo: PropTypes.object,
+	repos: PropTypes.array.isRequired,
+	starred: PropTypes.array.isRequired
 };
 
 export default AppContent;
