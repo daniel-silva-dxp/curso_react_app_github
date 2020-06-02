1dd6ac342717e1aae8d784a13b51f33e74e8013e
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-02 15:23:28 -0300

    feat: criando o component renderMessage

---
+++ src/components/renderMessage.js
@@ -0,0 +1,8 @@
+'use strict';
+import React from 'react';
+
+const RenderMessage = ({ children }) => {
+	return <div className="box-wrapper box-padding">{children}</div>;
+};
+
+export default RenderMessage;
