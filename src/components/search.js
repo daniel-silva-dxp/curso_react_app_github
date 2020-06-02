dae24e23f2df43eac6d8ef8350912cf320ccc6c6
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-02 10:13:18 -0300

    feat: fazendo request dos dados do user

--- src/components/search.js
+++ src/components/search.js
@@ -4,7 +4,21 @@ import React from 'react';
 const Search = () => {
 	return (
 		<div className="search box-wrapper">
-			<input type="search" placeholder="Digite o nome do usuário do Github" />
+			<input
+				type="search"
+				placeholder="Digite o nome do usuário do Github"
+				onKeyUp={(e) => {
+					const value = e.target.value;
+					const keyCode = e.which || e.keyCode;
+					const ENTER = 13;
+
+					if (keyCode === ENTER) {
+						fetch(`https://api.github.com/users/${value}`).then((data) => data.json()).then((data) => {
+							console.log(data.login);
+						});
+					}
+				}}
+			/>
 		</div>
 	);
 };
